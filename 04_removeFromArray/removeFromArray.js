a541d25d6e1a307dd83aec4307a1e2c9a74c913e
adeliya21 <adeliya.salieva@gmail.com>
2023-07-05 14:30:53 -0700

    Solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) { //any number of arguments using the rest parameter syntax
+    //solution using .forEach method
+    let newArray = [];
+    inputArray.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 
+    //solution using .filter method
+    //return inputArray.filter(val => !args.includes(val));   
 };
 
 // Do not edit below this line
