68a4c56b253cda39eb9c9cd2ff4c2932afce90a3
adeliya21 <adeliya.salieva@gmail.com>
2023-07-05 20:19:39 -0700

    Solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  let addition = n1 + n2;
+	return addition;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	let subtraction = n1 - n2;
+  return subtraction;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	let sum = 0;
+    for (let i = 0; i < inputArray.length; i++) sum += +inputArray[i]; // unary plus same as parseInt(inputArray[i]) or Number(inputArray[i])
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let multiplication = 1;
+  for (let i = 0; i < args.length; i++) multiplication *= +args[i]; // unary plus same as parseInt(args[i]) or number(args[i])
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let powResult = Math.pow(base, power);
+  return powResult;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  //recursive
+	if (num == 0 || num == 1) return 1;
+  else return num * factorial(num-1);
+
+  //iterative
+  // let result = 1;
+  // for (let i = 2; i < num; i++) { result *= i; }
+  // return result;
 };
 
 // Do not edit below this line
