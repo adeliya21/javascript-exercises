d5b48d0021839f80c68cb526da3465d3c2e6d553
adeliya21 <adeliya.salieva@gmail.com>
2023-07-05 17:09:51 -0700

    Add another way to check if input is number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if (n1 < 0 || n2 < 0 || typeof n1 !== 'number' || typeof n2 !== 'number') return 'ERROR'
+    // if (!Number.isInteger(n1) || !Number.isInteger(n2)) also works
+    const min = Math.min(n1, n2);
+    const max = Math.max(n1, n2);
+    let sum = ((max - min + 1) * (min + max)) / 2;
+    return sum;
 };
 
 // Do not edit below this line
