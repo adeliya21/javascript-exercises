f3962935a2aaf61e890336ec2299ebbc893c5582
adeliya21 <adeliya.salieva@gmail.com>
2024-01-27 11:51:43 -0800

    Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, ''); // ^ - not, [] - calss of alphanumeric chrs, g - replace globally 
+    return cleanString === cleanString.split('').reverse().join('');
 };
 
 // Do not edit below this line
